f10e191d1bc21792e152b5bd37b1acecdd8c2c10
Frederico Castro <castro.112358@gmail.com>
2014-09-22 13:38:56 -0400

    created inventory test

---
+++ soda_machine/src/edu/suny/np/soda/tests/InventoryTest.java
@@ -0,0 +1,29 @@
+package edu.suny.np.soda.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InventoryTest {
+
+	@Test
+	public void testAddToInventoryIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToInventoryStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateInventory() {
+		fail("Not yet implemented");
+	}
+
+}
