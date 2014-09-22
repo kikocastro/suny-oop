b798daf31e4442fd0bfeb9c6bd2d6e6b505614ac
Frederico Castro <castro.112358@gmail.com>
2014-09-22 14:08:12 -0400

    inventory not found exception

---
+++ soda_machine/src/edu/suny/np/exceptions/InventoryItemNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.suny.np.exceptions;
+
+public class InventoryItemNotFoundException extends Exception {
+	public InventoryItemNotFoundException(String message) {
+        super(message);
+	}
+}
